8a7d7d2327c9b1cd509cf19805137954e373fd3c
shakthi241 <shakthi.seshadri@fedo.ai>
2024-02-28 15:20:04 +0530

    added labout and fixed home page.

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
-import { CarouselComponent } from './cover-page/carousel/carousel.component';
 import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
 import { NowInOttComponent } from './now-in-ott/now-in-ott.component';
-import { NowInTheatresComponent } from './now-in-theatres/now-in-theatres.component';
-import { TopRegionalShowsComponent } from './top-regional-shows/top-regional-shows.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
   standalone: true,
-  imports:[CarouselComponent, NowInOttComponent, NowInTheatresComponent, TopRegionalShowsComponent, ImageCarouselComponent]
+  imports: [NowInOttComponent, ImageCarouselComponent]
 })
 export class HomePageComponent {
 
@@ -20,8 +17,10 @@ export class HomePageComponent {
   }
 
   ngOnInit() {
-    this.meta.addTag({ name: 'description', 
-    content: 'This is an article about Angular Meta service' }, true);
+    this.meta.addTag({
+      name: 'description',
+      content: 'This is an article about Angular Meta service'
+    }, true);
   }
 
 }
