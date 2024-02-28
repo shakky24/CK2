90da0acaf5f2fe5fa4642be29a87003c4eb93e3f
shakthi241 <shakthi.seshadri@fedo.ai>
2024-02-28 15:13:41 +0530

    added labout and fixed home page

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,23 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 
-import { HomePageRoutingModule } from './home-page-routing.module';
-import { HomePageComponent } from './home-page.component';
-import { NowInTheatresComponent } from './now-in-theatres/now-in-theatres.component';
-import { TopRegionalShowsComponent } from './top-regional-shows/top-regional-shows.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { AdThumbnailComponent } from './ad-thumbnail/ad-thumbnail.component';
 import { CarouselComponent } from './cover-page/carousel/carousel.component';
 import { FirstCoverComponent } from './cover-page/carousel/first-cover/first-cover.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MovieNewInTheatresComponent } from './now-in-theatres/movie-new-in-theatres/movie-new-in-theatres.component';
-import { MovieTopRegionalShowsComponent } from './top-regional-shows/movie-top-regional-shows/movie-top-regional-shows.component';
-import { MatIconModule } from '@angular/material/icon';
-import { UpcomingMoviesComponent } from './upcoming-movies/upcoming-movies.component';
-import { NowInOttComponent } from './now-in-ott/now-in-ott.component';
-import { AdThumbnailComponent } from './ad-thumbnail/ad-thumbnail.component';
-import { VerticalCarouselComponent } from './vertical-carousel/vertical-carousel.component';
+import { HomePageRoutingModule } from './home-page-routing.module';
+import { HomePageComponent } from './home-page.component';
 import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { NowInOttComponent } from './now-in-ott/now-in-ott.component';
+import { MovieNewInTheatresComponent } from './now-in-theatres/movie-new-in-theatres/movie-new-in-theatres.component';
+import { NowInTheatresComponent } from './now-in-theatres/now-in-theatres.component';
+import { MovieTopRegionalShowsComponent } from './top-regional-shows/movie-top-regional-shows/movie-top-regional-shows.component';
+import { TopRegionalShowsComponent } from './top-regional-shows/top-regional-shows.component';
+import { UpcomingMoviesComponent } from './upcoming-movies/upcoming-movies.component';
+import { VerticalCarouselComponent } from './vertical-carousel/vertical-carousel.component';
 
 
 @NgModule({
